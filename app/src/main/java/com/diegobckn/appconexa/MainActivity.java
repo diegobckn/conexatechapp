21dde7f8ee1aa66602ef28405ed9f94bf0b00754
diegobckn <diego.bckn@gmail.com>
2020-08-28 16:48:59 -0300

    Un poco de estilos

--- app/src/main/java/com/diegobckn/appconexa/MainActivity.java
+++ app/src/main/java/com/diegobckn/appconexa/MainActivity.java
@@ -1,14 +1,22 @@
 package com.diegobckn.appconexa;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Objects.requireNonNull(getSupportActionBar()).setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.mipmap.ic_launcher);
     }
 }
